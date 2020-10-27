7fd2822d4938601f05b371af0ce897380d67677b
balak143 <balakrishna.billa@gmail.com>
2020-10-27 18:25:40 +0530

    1) Enabled Distributed tracing using Slueth, Zipkin, AMQP libraries 2) Install Rabbit MQ as service 3) RUN Zipkin server using zipkin-server-2.19.3.jar set RABBIT_URI=amqp://localhost java -jar zipkin-server-2.19.3.jar

--- src/main/java/com/learn/restfulwebservices/restapi/VacationRestApiApplication.java
+++ src/main/java/com/learn/restfulwebservices/restapi/VacationRestApiApplication.java
@@ -1,10 +1,12 @@
 package com.learn.restfulwebservices.restapi;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -13,5 +15,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class VacationRestApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(VacationRestApiApplication.class, args);
+    }
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
